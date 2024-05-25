a8b6cb4f36c23d05216b2de9b05fc1fa1d90c2f2
fasal <fasal>
2024-05-26 02:17:36 +0530

    new UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,27 @@ import axios from 'axios';
 
 function App() {
 
-  const [ channels,setChannels ] = useState<any>([])
+  // const [ channels,setChannels ] = useState<any>([])
+  const [ users,setUsers ] = useState<any>([])
   const [ teams,setTeams ] = useState<any>([])
   const [ loader,setLoader ] = useState<boolean>(true)
+  const [ userPosts,setUserposts ] = useState<any>({})
+  
+  if(Object.values(userPosts)?.length) 
+  loader && setLoader(false)
 
-  let  postCount = 0
-  clctTtlPstCount()
+  // let  postCount = 0
+  // clctTtlPstCount()
   const baseUrl = "http://68.183.88.126:8065";
   const token = "7sacw7gn7b8xfx3wyeokrwqtfc";
   const userId = "kggwskrnatg48pusqf95sugatr";
 
   const headers = { "Authorization": `Bearer ${token}` };
+
+  async function getUsers() {
+    const response = await axios.get(`${baseUrl}/api/v4/users`, { headers })
+    setUsers(response.data)
+  }
 
   // getting the no.of teams of the user
   async function getUserTeams(userId: string) {
@@ -36,32 +46,58 @@ function App() {
   }
 
   async function countUserPosts() {
-    let channelsArr = []
+    // let channelsArr = []
+    let usrPsts: any = {}
     for (const team of teams) {
       const channels = await getUserChannels(userId, team.id);
       for (const chnl of channels) {
         const posts = await getChannelPosts(chnl.id);
-        const psts = Object.values(posts).filter((post: any) => post.user_id === userId && !['system_join_channel','system_join_team'].includes(post.type));
-        chnl.postCount = psts?.length || 0
-        channelsArr.push(chnl)
+        for(let [_key,value] of Object.entries(posts)){
+          const v = value as any
+          if(!usrPsts[v.user_id]) usrPsts[v.user_id] = 1
+          else usrPsts[v.user_id] += 1
+        }
+        // const psts = Object.values(posts).filter((post: any) => post.user_id === userId && !['system_join_channel','system_join_team'].includes(post.type));
+        // chnl.postCount = posts?.length || 0
+        // channelsArr.push(chnl)
       }
     }
-    setChannels(channelsArr)
+    setUserposts({
+      ...userPosts,
+      ...usrPsts
+    })
+    // setChannels(channelsArr)
   }
 
-  function clctTtlPstCount(){
-    if(channels?.length){
-      for(let ch of channels){
-        postCount += ch.postCount
-      }
-      loader && setLoader(false)
-    }
-  }
-
+  // function clctTtlPstCount(){
+  //   if(channels?.length){
+  //     for(let ch of channels){
+  //       postCount += ch.postCount
+  //     }
+  //     loader && setLoader(false)
+  //   }
+  // }
 
   useEffect(() => {
     countUserPosts()
   }, [teams])
+
+  useEffect(() => {
+    // sorting the users array with highest to lowest
+    const userScoresArray: any = Object.entries(userPosts);
+    userScoresArray.sort(([, valueA] : any, [, valueB]: any) => valueB - valueA);
+    const userMap = new Map(users.map((user: any) => [user.id, user]));
+    const sortedUsers = userScoresArray.map(([userId]: any) => userMap.get(userId));
+    const usersWithoutPosts = []
+    for(let uwp of users){
+      if(!userPosts[uwp.id]) usersWithoutPosts.push(uwp)
+    }
+    let finalUsers = [
+      ...sortedUsers,
+      ...usersWithoutPosts
+    ]
+    setUsers(finalUsers)
+  }, [userPosts])
 
   useEffect(() => {
     async function getTeams(){
@@ -69,13 +105,14 @@ function App() {
       setTeams(teams)
     }
     
+    getUsers()
     getTeams()
   }, [])
   
 
   return (
     <div className="parent">
-      <div className="card-wrapper">
+      {/* <div className="card-wrapper">
         {loader ? (
           "Loading..."
         ) : (
@@ -105,7 +142,21 @@ function App() {
             </div>
           </>
         )}
-      </div>
+      </div> */}
+      {loader ? (
+        "Loading..."
+      ) : (
+        <div className="card">
+          <div className="card-item green">User email</div>
+          <div className="card-item green">Posts</div>
+          {users.map((usr: any) => (
+            <>
+              <div className="card-item">{usr.email}</div>
+              <div className="card-item">{userPosts?.[usr.id] || 0}</div>
+            </>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
